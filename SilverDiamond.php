773d860d327d883a85d7e7e982adbea103ac00a7
Raúl López <hi@raullg.com>
2020-03-14 20:06:32 +0000

    Update SilverDiamond.php

--- SilverDiamond.php
+++ SilverDiamond.php
@@ -25,11 +25,6 @@ class Sentiment {
     const VERY_NEGATIVE = 'Very negative';
 }
 
-class Spam {
-    const SPAM = 'spam';
-    const HAM = 'ham';
-}
-
 class Api {
     private static $ENDPOINT = 'https://api.silverdiamond.io/api/service/';
     private $key;
